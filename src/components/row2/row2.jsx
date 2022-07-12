b1206fb4602b71a970ebc38fec462337460efb02
Sandeep Kumar Murmu <sandeep.16murmu@gmail.com>
2022-07-12 20:16:08 +0530

    few more updates

--- src/components/row2/row2.jsx
+++ src/components/row2/row2.jsx
@@ -1,13 +1,22 @@
 // connnecting css file
 import "./row2.css";
 
+// importing components
+import { Tags } from "../tag/tag";
+
 // row2 component
 export const Row2 = () => {
   return (
     <div className="row2">
-      <div className="row2_child1"></div>
+      <div className="row2_child1">
+        <div className="row2_grandChild1">
+          <Tags />
+        </div>
+        <div className="row2_grandChild2"></div>
+        <div className="row2_grandChild3"></div>
+      </div>
       <div className="row2_child2"></div>
-      <div className="row2_child1"></div>
+      <div className="row2_child1 "></div>
     </div>
   );
 };
